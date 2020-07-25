74544895ccb321ffbd8c7cc1ba9a5a9706de0da3
Charles <66225581+Quezard@users.noreply.github.com>
2020-07-25 22:05:28 +1200

    Update and rename prototype to prototype.py

--- prototype.py
+++ prototype.py
@@ -1,4 +1,4 @@
-import telebot
+import telebot                  #import telebot libary
 from telebot import types
 
 bot = telebot.TeleBot("992300889:AAEuMc0EdcBm7enrID7FDUjVuEzo9WqHbzM")
@@ -15,23 +15,24 @@ commands = {  # command description for new users
 }
 
 markup = types.ReplyKeyboardMarkup(row_width=2)
-itembtn1 = types.KeyboardButton('help')
-itembtn2 = types.KeyboardButton('v')
-itembtn3 = types.KeyboardButton('d')
+itembtn1 = types.KeyboardButton('/help')
+itembtn2 = types.KeyboardButton('/needhelp')
+itembtn3 = types.KeyboardButton('/helper')
 markup.add(itembtn1, itembtn2, itembtn3)
-tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
+
 
 # handle the "/start" command
 @bot.message_handler(commands=['start'])
 def command_start(m):
     cid = m.chat.id
-    if cid not in other:  # if user hasn't used the "/start" command yet:
+    if cid not in other and cid not in need_help and cid not in helpers:  # if user hasn't used the "/start" command yet:
         bot.send_message(cid, "Hello, stranger, ")
         bot.send_message(cid, "Scanning complete, I know you now")
         other.append(cid)
         command_help(m)  # show the new user the help page
     else:
         bot.send_message(cid, "I already know you, no need for me to scan you again!")
+        bot.send_message(cid, "Choose a command:", reply_markup=markup)
 
 
 # help page
@@ -43,13 +44,14 @@ def command_help(m):
         help_text += "/" + key + ": "
         help_text += commands[key] + "\n"
     bot.send_message(cid, help_text)  # send the generated help page
+    bot.send_message(cid, "Choose a command:", reply_markup=markup)
 
 #register as helper
 @bot.message_handler(commands=['helper'])
 def helping(m):
     cid = m.chat.id
     if cid in other:
-        other.pop(cid)
+        other.remove(cid)
         #take user contact details
         
     elif cid in need_help:
@@ -58,13 +60,14 @@ def helping(m):
     
     elif cid in helpers:
         bot.send_message(cid, "You are already a helper")
+    bot.send_message(cid, "Choose a command:", reply_markup=markup)
 
 #register as vulnerable
 @bot.message_handler(commands=['needhelp'])
 def helping(m):
     cid = m.chat.id
     if cid in other:
-        other.pop(cid)
+        other.remove(cid)
         #take user contact details
         
     elif cid in helpers:
@@ -73,5 +76,6 @@ def helping(m):
     
     elif cid in need_help:
         bot.send_message(cid, "You are already registered")
-        
+    bot.send_message(cid, "Choose a command:", reply_markup=markup)
+
 bot.polling()
